97435cfcda4558a4e6b556c6dcf03064dee32a19
Ar Rafi Uddin <arrafi.mahin@gmail.com>
2023-05-01 23:51:08 +0600

    003350

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import {  createTheme } from '@mui/material/styles'
+import {CssBaseline, ThemeProvider} from '@mui/material'
+import {useMemo} from 'react'
+import { themeSettings } from './Theme'
 function App() {
-  const [count, setCount] = useState(0)
+  const theme = useMemo(()=>
+    createTheme(themeSettings)
+  ,[])
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      hello world
+      </ThemeProvider>
+        
+    </div>
   )
 }
 
