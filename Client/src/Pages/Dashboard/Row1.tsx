749b9f4d7db9635433d333aeb17fdcbae2e28bfd
arrafi-mahin <48198243+arrafi-mahin@users.noreply.github.com>
2023-05-03 17:35:54 +0600

    24741

--- Client/src/Pages/Dashboard/Row1.tsx
+++ Client/src/Pages/Dashboard/Row1.tsx
@@ -1,16 +1,123 @@
-
+import {useMemo} from 'react'
 import DashboardBox from '../../Components/DashboardBox'
 import { useGetKpisQuery } from '../../state/api'
+import { ResponsiveContainer, AreaChart, CartesianGrid, XAxis, YAxis, Tooltip, Area } from 'recharts';
+import { useTheme } from '@mui/material';
+import BoxHeader from '../../Components/BoxHeader';
 type Props = {}
 
 function Row1({}: Props) {
+  const {palette} = useTheme();
   const {data } = useGetKpisQuery();
   console.log("data", data);
+
+  const revenueExpenses = useMemo(()=> {
+    return(
+      data && data[0].monthlyData.map(({month, revenue, expenses})=>{
+        return {
+          name: month.substring(0,3),
+          revenue: revenue,
+          expenses: expenses
+        }
+      })
+    )
+  },[data])
   return (
     <>
-        <DashboardBox  gridArea="a"></DashboardBox>
-        <DashboardBox  gridArea="b"></DashboardBox>
-        <DashboardBox  gridArea="c"></DashboardBox>
+        <DashboardBox  gridArea="a">
+          <BoxHeader title="Revenue and Expenses" subtitle='Top line represents revenue, bottom line represents expenses' sideText='+4%' />
+          <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          width={500}
+          height={400}
+          data={revenueExpenses}
+          margin={{
+            top: 15,
+            right: 25,
+            left: -10,
+            bottom: 60,
+          }}
+        >
+          <defs>
+            <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.5} />
+              <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.5} />
+              <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" tickLine={false} style={{fontSize: "10px"}} />
+          <YAxis axisLine={{strokeWidth: "0" }} tickLine={false} style={{fontSize: "10px"}} domain={[8000, 23000]} />
+          <Tooltip />
+          <Area type="monotone" dot={true} dataKey="revenue" stroke={palette.primary.main} fillOpacity={1} fill="url(#colorRevenue)" />
+          <Area type="monotone" dot={true} dataKey="expenses" stroke={palette.primary.main} fillOpacity={1} fill="url(#colorExpenses)" />
+        </AreaChart>
+          </ResponsiveContainer>
+        </DashboardBox>
+        <DashboardBox  gridArea="b">
+          <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          width={500}
+          height={400}
+          data={revenueExpenses}
+          margin={{
+            top: 15,
+            right: 25,
+            left: -10,
+            bottom: 60,
+          }}
+        >
+          <defs>
+            <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.5} />
+              <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.5} />
+              <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" tickLine={false} style={{fontSize: "10px"}} />
+          <YAxis axisLine={{strokeWidth: "0" }} tickLine={false} style={{fontSize: "10px"}} domain={[8000, 23000]} />
+          <Tooltip />
+          <Area type="monotone" dot={true} dataKey="revenue" stroke={palette.primary.main} fillOpacity={1} fill="url(#colorRevenue)" />
+          <Area type="monotone" dot={true} dataKey="expenses" stroke={palette.primary.main} fillOpacity={1} fill="url(#colorExpenses)" />
+        </AreaChart>
+          </ResponsiveContainer>
+        </DashboardBox>
+        <DashboardBox  gridArea="c">
+          <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          width={500}
+          height={400}
+          data={revenueExpenses}
+          margin={{
+            top: 15,
+            right: 25,
+            left: -10,
+            bottom: 60,
+          }}
+        >
+          <defs>
+            <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.5} />
+              <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.5} />
+              <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" tickLine={false} style={{fontSize: "10px"}} />
+          <YAxis axisLine={{strokeWidth: "0" }} tickLine={false} style={{fontSize: "10px"}} domain={[8000, 23000]} />
+          <Tooltip />
+          <Area type="monotone" dot={true} dataKey="revenue" stroke={palette.primary.main} fillOpacity={1} fill="url(#colorRevenue)" />
+          <Area type="monotone" dot={true} dataKey="expenses" stroke={palette.primary.main} fillOpacity={1} fill="url(#colorExpenses)" />
+        </AreaChart>
+          </ResponsiveContainer>
+        </DashboardBox>
     </>
   )
 }
--- Client/src/state/api.ts
+++ Client/src/state/api.ts
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
-
+import { GetKpisResponse } from "./types";
 export const api = createApi({
-    baseQuery: fetchBaseQuery({baseUrl: import.meta.env.BASE_URL}),
+    baseQuery: fetchBaseQuery({baseUrl: import.meta.env.VITE_BASE_URL}),
     reducerPath: "main",
     tagTypes: ["Kpis"],
     endpoints: (build)=>({
-        getKpis: build.query<void, void>({
+        getKpis: build.query<Array<GetKpisResponse>, void>({
             query: ()=> "kpi/kpis",
             providesTags: ["Kpis"]
         })
